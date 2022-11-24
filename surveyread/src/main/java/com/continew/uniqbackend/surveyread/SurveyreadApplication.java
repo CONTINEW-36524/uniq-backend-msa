f999b7d2c72ba5d05d1228144fc4c2b0e18892a0
Dajeong Park <jul897813@gmail.com>
2022-11-25 02:38:26 +0900

    Register survey-read-service to eureka server

--- surveyread/src/main/java/com/continew/uniqbackend/surveyread/SurveyreadApplication.java
+++ surveyread/src/main/java/com/continew/uniqbackend/surveyread/SurveyreadApplication.java
@@ -2,6 +2,7 @@ package com.continew.uniqbackend.surveyread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
