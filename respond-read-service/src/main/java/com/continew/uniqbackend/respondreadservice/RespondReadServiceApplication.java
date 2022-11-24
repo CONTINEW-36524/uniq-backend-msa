0832771483f2370ee3f25e5847ee126ab38524a5
Dajeong Park <jul897813@gmail.com>
2022-11-25 02:24:04 +0900

    Register respond-read-service to eureka server

--- respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/RespondReadServiceApplication.java
+++ respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/RespondReadServiceApplication.java
@@ -2,12 +2,14 @@ package com.continew.uniqbackend.respondreadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Configuration
 @EnableScheduling
+@EnableEurekaClient
 public class RespondReadServiceApplication {
 
 	public static void main(String[] args) {
