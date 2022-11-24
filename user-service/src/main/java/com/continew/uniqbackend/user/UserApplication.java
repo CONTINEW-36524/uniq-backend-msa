dc1ff4b6eddde02821c21cae11d0e20f6244f4de
Dajeong Park <jul897813@gmail.com>
2022-11-25 02:45:37 +0900

    Register user-service to eureka server

--- user-service/src/main/java/com/continew/uniqbackend/user/UserApplication.java
+++ user-service/src/main/java/com/continew/uniqbackend/user/UserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationShutdownHandlers;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
