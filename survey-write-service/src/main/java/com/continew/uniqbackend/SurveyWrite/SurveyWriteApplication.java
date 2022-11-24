6b0e4107d7c2894d754c0263953785f76feb226d
Dajeong Park <jul897813@gmail.com>
2022-11-25 02:30:31 +0900

    Register survey-write-service to eureka server

--- survey-write-service/src/main/java/com/continew/uniqbackend/SurveyWrite/SurveyWriteApplication.java
+++ survey-write-service/src/main/java/com/continew/uniqbackend/SurveyWrite/SurveyWriteApplication.java
@@ -2,6 +2,7 @@ package com.continew.uniqbackend.SurveyWrite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
