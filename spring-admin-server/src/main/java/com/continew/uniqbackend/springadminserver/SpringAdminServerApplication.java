5427db3a52340ea3ec7a724073ae595ebc2fcf96
Dajeong Park <jul897813@gmail.com>
2022-11-25 20:13:31 +0900

    Create Admin Server

---
+++ spring-admin-server/src/main/java/com/continew/uniqbackend/springadminserver/SpringAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.continew.uniqbackend.springadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminServerApplication.class, args);
+	}
+
+}
