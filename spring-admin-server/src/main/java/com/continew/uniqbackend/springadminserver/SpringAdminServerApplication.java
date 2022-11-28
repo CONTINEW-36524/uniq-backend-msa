6774de6e88f4c98cd342d93214cb4ab5e5524402
Dajeong Park <jul897813@gmail.com>
2022-11-28 22:07:58 +0900

    Add Security for Admin Server
    port 9090 & Security for

--- spring-admin-server/src/main/java/com/continew/uniqbackend/springadminserver/SpringAdminServerApplication.java
+++ spring-admin-server/src/main/java/com/continew/uniqbackend/springadminserver/SpringAdminServerApplication.java
@@ -1,9 +1,13 @@
 package com.continew.uniqbackend.springadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAdminServer
 @SpringBootApplication
+@EnableScheduling // notification 사용시 필요
 public class SpringAdminServerApplication {
 
 	public static void main(String[] args) {
