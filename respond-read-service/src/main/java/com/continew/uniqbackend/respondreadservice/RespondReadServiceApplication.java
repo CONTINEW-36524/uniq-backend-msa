76fc77f6a3767467ce8bb7a7fe9904a77914e105
vbnm134678 <76089508+vbnm134678@users.noreply.github.com>
2022-11-29 19:04:33 +0900

    respond read

--- respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/RespondReadServiceApplication.java
+++ respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/RespondReadServiceApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Configuration
 @EnableScheduling
 @EnableEurekaClient
+@RestController
 public class RespondReadServiceApplication {
 
+	@RequestMapping("/test")
+	public String testRR(){
+		return "Respond Read Service activate";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RespondReadServiceApplication.class, args);
 	}
--- respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/controller/RespondreadController.java
+++ respond-read-service/src/main/java/com/continew/uniqbackend/respondreadservice/controller/RespondreadController.java
@@ -2,12 +2,15 @@ package com.continew.uniqbackend.respondreadservice.controller;
 
 import com.continew.uniqbackend.respondreadservice.RespondReadServiceApplication;
 import com.continew.uniqbackend.respondreadservice.entity.RespondE;
-import com.continew.uniqbackend.respondreadservice.dto.Respond;
 import com.continew.uniqbackend.respondreadservice.repository.RespondERepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -20,7 +23,7 @@ public class RespondreadController {
     @Autowired
     private RespondERepository respondERepository;
 
-    @GetMapping("/respond/read/answer")
+    @GetMapping("/read/answer")
     public List<RespondE> getPopular(){
         List<RespondE> responds = respondERepository.selectRes();
         log.info(responds.toString());
